64a32e3eabdee9e94498a07420f7a3b2745d5cac
anish-stack <anishjha896@gmail.com>
2025-01-06 16:20:15 +0530

    first commit

--- controllers/vendor.controller.js
+++ controllers/vendor.controller.js
@@ -645,9 +645,7 @@ exports.loginVendor = async (req, res) => {
         const cookieOptions = {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            expires: new Date(
-                Date.now() + process.env.JWT_COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
-            ),
+           
         };
 
         res.status(200)
